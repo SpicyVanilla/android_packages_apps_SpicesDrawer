be539042ba438380295496209947315d86befad4
Funny-Bunny <dragos281993@gmail.com>
2017-06-30 14:58:21 +0300

    Add Quick Settings Pulldown options [2/2]
    Spicy Vanilla edits:
    * summaries created for both quick pulldown left & right
    * "always" option replaced with "both edges"
     - it looks miles better this way and it's a lot more specific
    
    * original author: romanbb
    
    Signed-off-by: Funny-Bunny <dragos281993@gmail.com>

--- src/com/spicyvanilla/settings/fragments/QSPanelSettings.java
+++ src/com/spicyvanilla/settings/fragments/QSPanelSettings.java
@@ -16,7 +16,13 @@
 
 package com.spicyvanilla.settings.fragments;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
@@ -24,17 +30,61 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class QSPanelSettings extends SettingsPreferenceFragment {
+public class QSPanelSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String QUICK_PULLDOWN = "quick_pulldown";
+
+    private ListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.qs_panel_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mQuickPulldown = (ListPreference) findPreference(QUICK_PULLDOWN);
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        int quickPulldownValue = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1, UserHandle.USER_CURRENT);
+        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+        updatePulldownSummary(quickPulldownValue);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.SPICESDRAWER;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQuickPulldown) {
+            int quickPulldownValue = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    quickPulldownValue, UserHandle.USER_CURRENT);
+            updatePulldownSummary(quickPulldownValue);
+            return true;
+        }
+        return false;
+    }
+
+    private void updatePulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else if (value == 1) {
+            // quick pulldown right
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary_right));
+        } else if (value == 2) {
+            // quick pulldown left
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary_left));
+        } else if (value == 3) {
+            // quick pulldown both edges
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary_both_edges));
+        }
     }
 }
